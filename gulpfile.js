b8239b65a8d481c7e6fb53616439a7b87a715e1f
tim zaycev <timon0596@yandex.ru>
2018-05-12 18:29:41 +0300

    menu

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,11 @@ gulp.task('sass',function() {
 	return gulp.src('app/sass/main.sass')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
-		.pipe(browsersync.reload({stream: true}))
+		
 });
 
-gulp.task('browser-sync',function(){
-	browsersync({
-		server: {
-			baseDir: 'app'
-		},
-		notyfy: false
-	});
-});
-gulp.task('watch',['browser-sync','sass'],function(){
+
+gulp.task('watch',['sass'],function(){
 	gulp.watch('app/sass/main.sass',['sass']);
-	gulp.watch('app/*.html', browsersync.reload);
-	gulp.watch('app/js/**/*.js', browsersync.reload);
+	
 });
